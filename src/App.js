e2c429ac01614fd16c6c857f3bacc46673c066fd
QuinnMcAuliffe <qxm00001@mix.evu.edu>
2024-02-25 15:00:46 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+        <div className="App">
+            <h1>CS 230L</h1>
+            <h2>Section - 001</h2>
+            <p>WVU ID: 800329659</p>
+            <p>Hi I am Quinn McAuliffe</p>
+        </div>
+    );
+}
+
+export default App;
